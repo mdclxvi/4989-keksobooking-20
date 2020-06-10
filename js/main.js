3e98219081f80e36fd9dea373e4199bff50ab579
alext <softlink@bk.ru>
2020-06-10 11:24:02 +0700

    первое задание личного проекта

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 'use strict';
 
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+
+var pinContainer = document.querySelector('.map__pins');
+var widthPinContainer = pinContainer.offsetWidth;
+
+var advertTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
 
 var avatars = [
   'img/avatars/user01.png',
@@ -19,42 +29,45 @@ var types = [
   'flat',
   'house',
   'bungalo',
-]
+];
 
-var createObjectAdvert = function () {
-  var advert = {
-    author: {
-      avatar: 'img/avatars/user{{xx}}.png'
-    },
-    offer: {
-      title: 'Заголовок предложения',
-      address: '600, 350',
-      price: '20',
-      type: '5',
-      rooms: '6',
-      guests: '7',
-      checkin: '8',
-      checkout: '9',
-      features: '10',
-      description: '11',
-      photos: '12'
-    },
-    location: {
-      x: '13',
-      y: '14',
-    }
-  };
-  return advert;
+var checkins = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+var checkouts = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+var features = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+var photos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+
+var advertCard = {
+  avatars: avatars,
+  types: types,
+  checkins: checkins,
+  checkouts: checkouts,
+  features: features,
+  photos: photos,
 };
 
-var createArrayAdverts = function (length) {
-  var adverts = [];
-  for (var i = 0; i < length; i++) {
-    adverts.push(createObjectAdvert());
-  }
-
-  return adverts;
-};
 
 var getRandomValue = function (max, min) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -65,8 +78,56 @@ var getRandomElement = function (arr) {
   return arr[randomIndex];
 };
 
-var haha = createObjectAdvert();
+var createArrayAdverts = function (countAdvert, widthContainer) {
 
-var ooops = getRandomValue(630, 130);
-console.log(ooops);
+  var adverts = [];
+  for (var i = 0; i < countAdvert; i++) {
+    var advert = {
+      author: {
+        avatar: avatars[i]
+      },
+      offer: {
+        title: 'Заголовок предложения',
+        address: '600, 350',
+        price: '20',
+        type: getRandomElement(types),
+        rooms: 3,
+        guests: 7,
+        checkin: getRandomElement(checkins),
+        checkout: getRandomElement(checkouts),
+        features: getRandomElement(features),
+        description: '11',
+        photos: getRandomElement(photos)
+      },
+      location: {
+        x: getRandomValue(widthContainer, 0),
+        y: getRandomValue(360, 130),
+      }
+    };
+    adverts.push(advert);
+  }
+
+  return adverts;
+};
+
+var renderPin = function (template, width, height, advert) {
+
+  var pinElement = template.cloneNode(true);
+  pinElement.style.left = advert.location.x - width / 2 + 'px';
+  pinElement.style.top = advert.location.y - height + 'px';
+  pinElement.querySelector('img').setAttribute('alt', advert.offer.title);
+  pinElement.querySelector('img').src = advert.author.avatar;
+
+  return pinElement;
+};
+
+var adverts = createArrayAdverts(8, widthPinContainer, advertCard);
+
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < adverts.length; i++) {
+  fragment.appendChild(renderPin(advertTemplate, PIN_WIDTH, PIN_HEIGHT, adverts[i]));
+}
+
+pinContainer.appendChild(fragment);
 
